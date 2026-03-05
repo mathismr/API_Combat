bd723c23a34e2fe2530e54924ee05e5bde41ece0
mathismr <mathis10r@outlook.fr>
2026-03-05 15:35:43 +0100

    move update combat to other router

--- app/api/endpoints/combat.py
+++ app/api/endpoints/combat.py
@@ -1,36 +1,23 @@
 from fastapi import APIRouter, Request, HTTPException
-from uuid import UUID
 
-from app.schemas.combat import CombatOut, CombatCreate, CombatUpdate
-from app.crud.CRUD_combat import create_combat, update_combat
+from app.schemas.combat import CombatOut, CombatCreate
+from app.crud.CRUD_combat import create_combat
 
 router = APIRouter()
 
+#
+# This route is called when user validates combat creation in web UI
+#
 @router.post(
-"/combat/new",
+"/new",
      response_model=CombatOut,
      responses={400: {"description": "Bad format for combat creation"}}
 )
 async def http_create_combat(request: Request, combat_in: CombatCreate):
-    db = request.app.db
+    db = request.app.combat_db
     created_combat = await create_combat(db, combat_in)
 
     if created_combat is None:
         raise HTTPException(status_code=400, detail="Bad format for combat model")
 
     return created_combat
-
-@router.patch(
-    "/combat/update/{combat_id}",
-    response_model=CombatOut,
-    responses={400: {"description": "Bad format for combat update"}}
-)
-async def http_update_combat(request: Request, combat_id: str, combat_in: CombatUpdate):
-    db = request.app.db
-
-    combat = await db.combats.find_one({"_id": UUID(combat_id)})
-    if not combat:
-        raise HTTPException(status_code=404, detail="Combat not found")
-
-    updated_combat = await update_combat(db, UUID(combat_id), combat_in)
-    return updated_combat
