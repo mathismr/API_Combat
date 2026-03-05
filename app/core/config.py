f4e89b72f006ccf92000627c01080c6e1e139cd1
mathismr <mathis10r@outlook.fr>
2026-03-05 15:26:29 +0100

    update env file support

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONOGODB_URI: str = "mongodb://localhost:27017"
     DATABASE_NAME: str = "combats"
+    MONSTERS_API_BASE_URL: str = None
+    MONSTERS_API_PORT: str = None
 
     class Config:
         env_file = ".env"
