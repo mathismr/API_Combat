e90ae9932e6db44b07b671432d0e0eac2c477700
mathismr <mathis10r@outlook.fr>
2026-03-05 15:27:04 +0100

    add external_requests.py

---
+++ app/utils/external_requests.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen, HTTPError
+from http.client import HTTPResponse
+from typing import Dict
+import json
+
+from app.core.config import settings
+
+MONSTERS_API_URL = f"{settings.MONSTERS_API_BASE_URL}:{settings.MONSTERS_API_PORT}/api/monsters"
+
+def monster_api_rq(endpoint: str, uuid: str) -> Dict:
+    match endpoint:
+        case "monster":
+            url = f"{MONSTERS_API_URL}/get/{uuid}"
+        case "skill":
+            url = f"{MONSTERS_API_URL}/skills/get/{uuid}"
+        case _:
+            return {"error": "Invalid endpoint"}
+
+    try:
+        print(url)
+        response: HTTPResponse = urlopen(url)
+        response_json = json.loads(response.read().decode("utf-8"))
+        return response_json
+    except HTTPError as e:
+        return json.loads(e.read().decode("utf-8"))
