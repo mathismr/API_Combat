1f479406ab49e2f04def10a8cbfc82e5c4713c04
mathismr <mathis10r@outlook.fr>
2026-03-27 16:22:17 +0100

    feat: add get route for turns

--- app/api/endpoints/turn.py
+++ app/api/endpoints/turn.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter, Request
 
-from app.crud.CRUD_turn import create_turn
+from app.crud.CRUD_turn import create_turn, get_turn
 from app.schemas.turn import TurnOut, TurnRequest
 
 router = APIRouter()
+
+
+@router.get(
+    "/get/{turn_id}",
+    response_model=TurnOut
+)
+async def http_get_turn(request: Request, turn_id: str):
+    db = request.app.combat_db
+    return await get_turn(db, turn_id)
 
 
 @router.post(
